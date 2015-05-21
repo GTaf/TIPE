eac50fafa49ba65cbe976151ad71d077927019dc
GTaf <auredu94@gmail.com>
2015-05-21 19:07:58 +0200

    Changement du calcul de primitive
    Méga rapide

--- Modèle.py
+++ Modèle.py
@@ -2,64 +2,87 @@ from calcul_T import *
 import scipy
 from math import *
 from scipy import interpolate
+import numpy as np
 
 
-def gamma_temperature(fluide,alpha,a):
+def gamma_temperature(fluide,alpha):
     """ Prend en entrée la nature du fluide : 0=air, 1=kerosene, 2= melange air-kerosene,
     la richesse alpha (valant 0 pour l'air et 1 pour le kerosene pur),
     la vitesse du son a.
     Renvoi gamma(T)"""
+    a = lambda T: exp(3090/T)
     if fluide==0:
-        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a(T))/(T*T*(a(T)-1)*(a(T)-1))
         gamma=lambda T: Cpr(T)/(Cpr(T)-1)
     if fluide==1:
         Cpr=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
         gamma=lambda T: Cpr(T)/(Cpr(T)-1)
     if fluide==2:
-        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a(T))/(T*T*(a(T)-1)*(a(T)-1))
         Cprk=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
         Cpr=lambda T: (Cpra(T)+alpha*Cprk(T))/(alpha+1)
         gamma=lambda T: Cpr(T)/(Cpr(T)-1)
     return gamma
     
-def Cp_temperature(fluide,alpha,a):
+def Cp_temperature(fluide,alpha):
     """ Prend en entrée la nature du fluide : 0=air, 1=kerosene, 2= melange air-kerosene,
     la richesse alpha (valant 0 pour l'air et 1 pour le kerosene pur),
-    la vitesse du son a.
+    la vitesse du son a. RIEN A VOIR
     Renvoi Cp(T)"""
+    a = lambda T: exp(3090/T)
     if fluide==0:
-        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpr=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a(T))/(T*T*(a(T)-1)*(a(T)-1))
     if fluide==1:
         Cpr=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
     if fluide==2:
-        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3000*3000*a)/(T*(a-1)*(a-1))
+        Cpra=lambda T: 3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a(T))/(T*T*(a(T)-1)*(a(T)-1))
         Cprk=lambda T: -0.0000018373*T*T+0.00801994*T+4.47659
         Cpr=lambda T: (Cpra(T)+alpha*Cprk(T))/(alpha+1)
     return Cpr
+#calcul des fonctions utilisés dans les blocs du turboréacteur
+
+
+
+a = lambda T: exp(3090/T)
+
+"""integrale de gamma sur gamma-1, pour l'air pur"""
+fp1=lambda T: gamma_temperature(0,0)(T)/(T*(gamma_temperature(0,0)(T)-1)) #fonction gamma/T*(gamma-1) de l'air
+F1=calcul_T(20,3000,1,fp1) #calcul de la primitive de gamma/T*(gamma-1) pour l'air
+    
+    
+f1 = interpolate.interp1d(F1[0],F1[1])#fonction primitive
+finv1=interpolate.interp1d(F1[1],F1[0]) #reciproque
+
+""" integrale des cp"""
+Cpra=lambda T: (3.5-0.000028*T+0.0000000224*T*T+(3090*3090*a(T))/(T*T*(a(T)-1)*(a(T)-1)))*298
+Cprk=lambda T: (-0.0000018373*T*T+0.00801994*T+4.47659)*298
+F2a = calcul_T(10,10000,1,Cpra)#passage long
+F2k = calcul_T(10,10000,1,Cprk)
+
+f2a = interpolate.interp1d(F2a[0],F2a[1])#fonction primitive
+finv2a=interpolate.interp1d(F2a[1],F2a[0]) #fonction l'inverse
+
+f2k = interpolate.interp1d(F2k[0],F2k[1])#fonction primitive
+finv2k=interpolate.interp1d(F2k[1],F2k[0]) #fonction l'inverse
+
+
     
 def turboreacteur(T1,P1,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a):
     """Modélisation d'un turboréacteur
-    Hypothèses : transformations isentropiques dans com  """
+    Hypothèses : transformations isentropiques dans com  """          
     
-    #calcul des fonctions utilisés dans les blocs du turboréacteur
-    fp1=lambda T: gamma_temperature(0,0,a)(T)/(T*(gamma_temperature(0,0,a)(T)-1)) #fonction gamma/T*(gamma-1) de l'air
-    F1=calcul_T(200,2500,1,fp1) #calcul de la primitive de gamma/T*(gamma-1) pour l'air
-    
-    
-    f1 = interpolate.interp1d(F1[0],F1[1])#fonction primitive
-    finv1=interpolate.interp1d(F1[1],F1[0]) #reciproque
-
-    fp2=lambda T : 287*Cp_temperature(2,alpha,a)(T) #fonction 287*Cp pour mélange air/essence
-    F2=calcul_T(200,2500,1,fp2) #calcul de la primitive de 287*Cp pour mélange air/essence
-    
-    f2 = interpolate.interp1d(F2[0],F2[1])#fonction primitive
-    finv2=interpolate.interp1d(F2[1],F2[0]) #fonction l'inverse            
+    f2 = lambda T : (f2a(T)+alpha*f2k(T))/(1+alpha)
+    F2 = (np.array(F2a)+alpha*np.array(F2k))/(1+alpha)
+    finv2=interpolate.interp1d(F2[1],F2[0])
+    print(finv2(f2(900)))
             
-    
+    print("T1 = ",T1)
     #Soufflante, obtenu par integration(Laplace adapté)
     T2=finv1(f1(T1)+log(ts)) #log correspond au logarithme néperien
+    print("T2 = ",T2)
     #Compresseur BP, obtenu par integration(Laplace adapté)
     T3=finv1(f1(T2)+log(tcbp))#log correspond au logarithme néperien
+    print("T3 = ",T3)
         
     #Compresseur HP, obtenu par integration(Laplace adapté)
     T4=finv1((f1(T3)+log(tchp)))#log correspond au logarithme néperien
@@ -71,30 +94,32 @@ def turboreacteur(T1,P1,ts,tcbp,tchp,tt,alpha,lamb,WA,WF,a):
     avanct=WF/(0.012+4*0.001)
     Df=avanct*(DfCO2+2*DfH2O-DfCH4)
     
-    print((T4))
+    print("T4 = ",T4,"      ",f2(T4))
     
-    T5=finv2(f2(T4)+Df)
+    T5=finv2(f2(T4)+(Df/(WA+WF)))
     
-    print(T5)
+    print("Température chambre = ",T5)
     
     #Turbine HP, obtenu par equilibre HP
-    T6=finv2(f2(T5)+(f2(T4)-f2(T3))*WA/(WA+WF))
+    T6=finv2(f2(T5)-(f2(T4)-f2(T3))*WA/(WA+WF))#problème rendement manquant
+    print("T6 = ",T6)
     
     #Turbine BP, obtenu par equilibre BP
-    T7=finv2(f2(T6)+(f2(T3)-f2(T1))*WA/(WA+WF)+(f2(T1)-f2(T2))*lamb*WA/(WA+WF))
+    T7=finv2(f2(T6)-0.7*0.7*(f2(T3)-f2(T1))*WA/(WA+WF)+0.7*0.7*(f2(T1)-f2(T2))*lamb*WA/(WA+WF))#rendement de 0.7, à revoir
+    print("T7 = ",T7)
     
     #Mélangeur, application 1er principe
     T8=finv2(((WA+WF)*f2(T7)+lamb*WA*f2(T2))/(WF+WA+WA*lamb))
     
     #Tuyère, application 1er principe
     T9=finv1(f1(T8)+log(tt))
-    C9=sqrt(2+f2(T9)-f2(T8))
+    C9=sqrt(2*(-f2(T9)+f2(T8)))
     
     #Rendement
     Pcin=(WA+lamb*WA+WF)*C9*C9/2
     Pth=(WA+WF)*(f2(T5)-f2(T4))
     Rendement=Pcin/Pth
     
-    return Rendement
+    return Rendement,T5
     
     
