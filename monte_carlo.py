9bcbae21e18186b97665d65ec5e1b7befbb414a4
GTaf <auredu94@gmail.com>
2015-05-22 11:14:40 +0200

    Uopdate

--- monte_carlo.py
+++ monte_carlo.py
@@ -17,21 +17,21 @@ def monte_carlo(N):
         experience=[]
         
         #Altitude
-        z=randint(0,32000)
+        z=10000#randint(0,32000)
         
         TC = ISA_temp(z)
         PC= ISA_P(z)
         
         #Nombre de Mach
-        M=choice([0,0.1,0.3,0.5,0.7,0.8,0.85,0.9,0.95,0.99])#choix au hasard
+        M=0.9#choice([0,0.1,0.3,0.5,0.7,0.8,0.85,0.9,0.95,0.99])#choix au hasard
         #Temperature totale
         TT=TC*(1+0.2*M**2)
         #Pression totale
         PT=PC*(1+0.2*M**2)**3.5
         #Taux de compression
-        ts=randint(1,3)#soufflante
-        tcbp=randint(1,3)#compresseur bp
-        tchp=randint(1,3)#compresseur hp
+        ts=1.6#randint(1,3)#soufflante
+        tcbp=3#randint(1,3)#compresseur bp
+        tchp=3#randint(1,3)#compresseur hp
         #Rendement
         rs=0.7#uniform(0.5,1)
         rcbp=0.7#uniform(0.5,1)
@@ -39,15 +39,15 @@ def monte_carlo(N):
         rtbp=0.7#uniform(0.5,1)
         rthp=0.7#uniform(0.5,1)
         #Alpha
-        alpha=uniform(0.01,0.06)
-        tt=0.97#turbine
+        alpha=uniform(0.06,0.06)
+        tt=1.5#turbine
         #Coefficient de partage du flux
-        lamb=random()*10
+        lamb=randint(1,10)
         #Flux
-        WA=randint(100,1000)
+        WA=1
         WF=alpha*WA
         #Vitesse
-        VA=M*(1.4*237*TC)**0.5      
+        VA=M*1224#(1.4*237*TC)**0.5      
         
         experience=[TT,PT,ts,tcbp,tchp,tt,rs,rcbp,rchp,rtbp,rthp,alpha,lamb,WA,WF,VA,turboreacteur(TT,PT,ts,tcbp,tchp,tt,rs,rcbp,rchp,rtbp,rthp,alpha,lamb,WA,WF,VA)]
         resultat.write(str(experience)+'\n')#ajoute au fichier
