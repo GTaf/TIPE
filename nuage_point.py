3f8d9ec573d9aa20d10bb137fa8aecd4a9f38b30
GTaf <auredu94@gmail.com>
2015-05-25 21:11:16 +0200

    Update nuage_point.py

--- nuage_point.py
+++ nuage_point.py
@@ -2,17 +2,14 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-def randrange(n, vmin, vmax):
-    return (vmax-vmin)*np.random.rand(n) + vmin
-
 fig = plt.figure()
 ax = Axes3D(fig)
-n = 100
+
 for i in L :
-    xs = randrange(n, 23, 32)
-    ys = randrange(n, 0, 100)
-    zs = randrange(n, zl, zh)
-    ax.scatter(xs, ys, zs, c=c, marker=m)
+    xs = i[0]
+    ys = i[1]
+    zs = i[2]
+    ax.scatter(xs, ys, zs)
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
